d1c0386985db7184b895e4a886672049b8c51130
jim <jimzh@hotmail.com>
2024-06-30 17:39:48 +1000

    Update .zshrc and other dotfiles

--- backup_dotfiles.sh
+++ backup_dotfiles.sh
@@ -3,4 +3,7 @@ cp -r ~/.config/i3 ./dotfiles/.config/
 echo "backing up picom config"
 cp -r ~/.config/picom ./dotfiles/.config/
 echo "backing up tmuxconf config"
-cp -r ~/.tmux.conf ./dotfiles
+cp -r ~/.tmux.conf ./dotfiles/
+
+echo "backing up zshrc config"
+cp -r ~/.zshrc ./dotfiles/
