9117aa20f3bad354de98f476e3c69fd10ac3549f
jim <jimzh@hotmail.com>
2024-12-25 21:27:03 +1100

    Added nvim configs

---
+++ dotfiles/.config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,11 @@
+return {
+  -- { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  -- vim.cmd.colorscheme("catppuccin"),
+  { "ellisonleao/gruvbox.nvim" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+}
---
+++ dotfiles/.config/nvim/lua/plugins/my_plugins.lua
@@ -0,0 +1,47 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "flake8",
+        "pyright",
+      },
+    },
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      window = {
+        position = "left",
+        width = 30,
+      },
+    },
+  },
+  -- add pyright to lspconfig
+  {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = {
+      ---@type lspconfig.options
+      servers = {
+        -- pyright will be automatically installed with mason and loaded with lspconfig
+        pyright = {},
+      },
+    },
+  },
+  require("telescope").setup({
+    defaults = {
+      -- prompt_prefix = " ",
+      -- selection_caret = " ",
+      path_display = { "truncate" },
+      -- dynamic_preview_title = true,
+      --     winblend = 10,
+      -- sorting_strategy = "descending",
+      -- layout_strategy = "vertical",
+      -- layout_config = {
+      --   prompt_position = "bottom",
+      --   height = 0.95,
+      -- },
+    },
+  }),
+}
