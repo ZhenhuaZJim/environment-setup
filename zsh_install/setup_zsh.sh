c70a1fda1808955ad0337d7320b5210f8a20d80c
Jim <jimzh@hotmail.com>
2024-12-26 17:16:19 +1100

    Update terminal theme

--- zsh_install/setup_zsh.sh
+++ zsh_install/setup_zsh.sh
@@ -7,3 +7,6 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 
+cp -r ../dotfiles/.zshrc ~/
+
+bash -c "$(wget -qO- https://git.io/vQgMr)"
