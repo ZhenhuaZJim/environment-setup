ff06fc88f046404511746647b92899a6eb28a575
Jim <jimzh@hotmail.com>
2024-12-26 13:30:44 +1100

    Added fzf install

--- nvim_install/nvim_install.sh
+++ nvim_install/nvim_install.sh
@@ -2,3 +2,5 @@ curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.
 sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux64.tar.gz
 sudo apt install npm
+
+sudo apt install fzf
