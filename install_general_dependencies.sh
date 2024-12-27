8efefa7b7574d4defb7edd4a88abb7547305ada1
Jim <jimzh@hotmail.com>
2024-12-27 14:13:27 +1100

    Update

--- install_general_dependencies.sh
+++ install_general_dependencies.sh
@@ -1,6 +1,6 @@
 sudo apt install libxcb-xtest0
 
-sudo apt install cmake curl npm
+sudo apt install cmake curl npm fzf
 
 curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
 sudo apt install nodejs -y
